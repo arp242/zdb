33e1a30e67cf93baad6ec195cd6b71d256ab8cfd
Martin Tournoij <martin@arp242.net>
2020-01-04 14:14:58 +0800

    Less ad-hoc DB migrations

---
+++ migrate.go
@@ -0,0 +1,150 @@
+package zdb
+
+// TODO: more advanced -migrate flag
+//   Show migration status and exit:             ./goatcounter -migrate
+//   Migrate all pending migrations and exit:    ./goatcounter -migrate all
+//   Migrate one and exit:                       ./goatcounter -migrate 2019-10-16-1-geoip
+//   Rollback last migration:                    ./goatcounter -migrate rollback:last
+//   Rollback specific migration:                ./goatcounter -migrate rollback:2019-10-16-1-geoip
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+
+	"github.com/pkg/errors"
+	"zgo.at/utils/sliceutil"
+)
+
+type Migrate struct {
+	DB          DB
+	Flag        string            // Value of -migrate flag.
+	Migrations  map[string][]byte // List of all migrations, for production.
+	MigratePath string            // Path to migrations, for dev.
+}
+
+func NewMigrate(db DB, flag string, mig map[string][]byte, path string) *Migrate {
+	return &Migrate{db, flag, mig, path}
+}
+
+// Run a migration, or all of then if which is "all".
+func (m Migrate) Run(which string) error {
+	haveMig, ranMig, err := m.List()
+	if err != nil {
+		return err
+	}
+
+	torun := []string{which}
+	if which == "all" {
+		torun = sliceutil.DifferenceString(haveMig, ranMig)
+	}
+
+	for _, run := range torun {
+		version := strings.TrimSuffix(filepath.Base(run), ".sql")
+		if sliceutil.InStringSlice(ranMig, version) {
+			return fmt.Errorf("migration already run: %q (version entry: %q)", run, version)
+		}
+
+		s, err := m.Schema(run)
+		if err != nil {
+			return err
+		}
+
+		l.Field("name", run).Print("SQL migration")
+		_, err = m.DB.ExecContext(context.Background(), s)
+		if err != nil {
+			return errors.Wrapf(err, "migrate %s", run)
+		}
+	}
+
+	return nil
+}
+
+// Get a list of all migrations we know about, and all migrations that have
+// already been run.
+func (m Migrate) List() (haveMig, ranMig []string, err error) {
+	if _, err := os.Stat(m.MigratePath); os.IsNotExist(err) {
+		// Compiled migrations.
+		for k := range m.Migrations {
+			haveMig = append(haveMig, k)
+		}
+	} else {
+		// Load from filesystem.
+		haveMig, err = filepath.Glob(m.MigratePath + "/*.sql")
+		if err != nil {
+			return nil, nil, errors.Wrap(err, "glob")
+		}
+	}
+	for i := range haveMig {
+		haveMig[i] = filepath.Base(haveMig[i][:len(haveMig[i])-4])
+	}
+	sort.Strings(haveMig)
+
+	err = m.DB.SelectContext(context.Background(), &ranMig,
+		`select name from version order by name asc`)
+	return haveMig, ranMig, errors.Wrap(err, "select version")
+}
+
+// Schema of a migration by name.
+func (m Migrate) Schema(n string) (string, error) {
+	if !strings.HasSuffix(n, ".sql") {
+		n += ".sql"
+	}
+
+	var (
+		b   []byte
+		err error
+	)
+	if _, serr := os.Stat(m.MigratePath); os.IsNotExist(serr) {
+		// Compiled migrations.
+		err = fmt.Errorf("no migration found: %q", n)
+		for k, v := range m.Migrations {
+			if strings.HasSuffix(k, n) {
+				b = v
+				err = nil
+				break
+			}
+		}
+	} else {
+		path := filepath.Clean(n)
+		if !strings.HasPrefix(path, m.MigratePath) {
+			path = filepath.Join(m.MigratePath, path)
+		}
+
+		// Load from filesystem.
+		b, err = ioutil.ReadFile(path)
+	}
+
+	return string(b), errors.Wrap(err, "Migrate.Schema")
+}
+
+// Check if there are pending migrations and zlog.Error() if there are.
+func (m Migrate) Check() error {
+	if m.Migrations == nil && m.MigratePath == "" {
+		return nil
+	}
+
+	if m.Flag != "" {
+		err := m.Run(m.Flag)
+		if err != nil {
+			return err
+		}
+	}
+
+	haveMig, ranMig, err := m.List()
+	if err != nil {
+		return err
+	}
+
+	if d := sliceutil.DifferenceString(haveMig, ranMig); len(d) > 0 {
+		l.Field("migrations", d).Errorf("pending migrations")
+	}
+	if d := sliceutil.DifferenceString(ranMig, haveMig); len(d) > 0 {
+		l.Field("migrations", d).Errorf("migrations in the DB that don't exist")
+	}
+	return nil
+}
--- zdb.go
+++ zdb.go
@@ -3,20 +3,16 @@ package zdb
 import (
 	"context"
 	"database/sql"
-	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
-	"sort"
-	"time"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
-	"zgo.at/utils/sliceutil"
 	"zgo.at/zlog"
 )
 
-var ctxkey = &struct{ n string }{"d"}
+// Date format for SQL.
+const Date = "2006-01-02 15:04:05"
 
 // DB wraps sqlx.DB so we can add transactions and logging.
 type DB interface {
@@ -24,12 +20,14 @@ type DB interface {
 	GetContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error
 	Rebind(query string) string
 	SelectContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error
-
-	// BeginTxx(ctx context.Context, opts *sql.TxOptions) (*sqlx.Tx, error)
-	// Rollback() error
-	// Commit() error
 }
 
+var (
+	ctxkey = &struct{ n string }{"d"}
+	l      = zlog.Module("zdb")
+)
+
+// With returns a copy of the context with the DB instance.
 func With(ctx context.Context, db DB) context.Context {
 	return context.WithValue(ctx, ctxkey, db)
 }
@@ -44,6 +42,9 @@ func MustGet(ctx context.Context) DB {
 }
 
 // Begin a new transaction.
+//
+// The returned context is a copy of the original with the DB replaced with a
+// transaction. This transaction is also returned directly.
 func Begin(ctx context.Context) (context.Context, *sqlx.Tx, error) {
 	// TODO: to supported nested transactions we need to wrap it.
 	// Also see: https://github.com/heetch/sqalx/blob/master/sqalx.go
@@ -56,133 +57,71 @@ func Begin(ctx context.Context) (context.Context, *sqlx.Tx, error) {
 	return context.WithValue(ctx, ctxkey, tx), tx, err
 }
 
+type ConnectOptions struct {
+	Connect    string // Connect string.
+	PostgreSQL bool   // Use PostgreSQL?
+	Schema     []byte // Database schema to create on startup.
+	Migrate    *Migrate
+}
+
 // Connect to database.
-func Connect(connect string, pgSQL bool, schema []byte, migrations map[string][]byte, migpath string) (*sqlx.DB, error) {
-	var db *sqlx.DB
-	if pgSQL {
-		var err error
-		db, err = sqlx.Connect("postgres", connect)
-		if err != nil {
-			return nil, errors.Wrap(err, "sqlx.Connect postgres")
-		}
-
-		// db.SetConnMaxLifetime()
-		db.SetMaxIdleConns(25) // Default 2
-		db.SetMaxOpenConns(25) // Default 0
+func Connect(opts ConnectOptions) (*sqlx.DB, error) {
+	var (
+		db     *sqlx.DB
+		exists bool
+		err    error
+	)
+	if opts.PostgreSQL {
+		db, exists, err = connectPostgreSQL(opts.Connect)
 	} else {
-		exists := true
-		if _, err := os.Stat(connect); os.IsNotExist(err) {
-			zlog.Printf("database %q doesn't exist; loading new schema", connect)
-			err = os.MkdirAll(filepath.Dir(connect), 0755)
-			if err != nil {
-				return nil, errors.Wrap(err, "create DB dir")
-			}
-			exists = false
-		}
-		var err error
-		db, err = sqlx.Connect("sqlite3", connect)
-		if err != nil {
-			return nil, errors.Wrap(err, "sqlx.Connect sqlite")
-		}
+		db, exists, err = connectSQLite(opts.Connect)
+	}
+	if err != nil {
+		return nil, err
+	}
 
-		if !exists {
-			db.MustExec(string(schema))
-			err := runmig(db, migrations, migpath, "all")
+	if opts.Migrate != nil {
+		opts.Migrate.DB = db
+	}
+
+	if !exists {
+		l.Printf("database %q doesn't exist; loading new schema", opts.Connect)
+		db.MustExec(string(opts.Schema))
+		if opts.Migrate != nil {
+			err := opts.Migrate.Run("all")
 			if err != nil {
 				return nil, errors.Wrap(err, "migration")
 			}
 		}
 	}
 
-	return db, checkmig(db, migrations, migpath)
+	if opts.Migrate != nil {
+		err = opts.Migrate.Check()
+	}
+	return db, err
 }
 
-func runmig(db *sqlx.DB, migrations map[string][]byte, migpath string, which string) error {
-	torun := []string{which}
-	if which == "all" {
-		haveMig, ranMig, err := lsmig(db, migrations, migpath)
-		if err != nil {
-			return err
-		}
-
-		torun = sliceutil.DifferenceString(haveMig, ranMig)
-	}
-
-	for _, run := range torun {
-		f, err := ioutil.ReadFile(fmt.Sprintf("%s/%s.sql", migpath, run))
-		if err != nil {
-			return err
-		}
-
-		_, err = db.Exec(string(f))
-		if err != nil {
-			return errors.Wrapf(err, "migrate %s", run)
-		}
-	}
-
-	return nil
-}
-
-func lsmig(db *sqlx.DB, migrations map[string][]byte, migpath string) (haveMig, ranMig []string, err error) {
-	// Check migrations.
-	if _, err := os.Stat(migpath); os.IsNotExist(err) {
-		for k := range migrations {
-			haveMig = append(haveMig, k)
-		}
-	} else {
-		haveMig, err = filepath.Glob(migpath + "/*.sql")
-		if err != nil {
-			return nil, nil, errors.Wrap(err, "glob")
-		}
-	}
-	for i := range haveMig {
-		haveMig[i] = filepath.Base(haveMig[i][:len(haveMig[i])-4])
-	}
-	sort.Strings(haveMig)
-
-	err = db.Select(&ranMig, `select name from version order by name asc`)
-	return haveMig, ranMig, errors.Wrap(err, "select version")
-}
-
-// Check if there are pending migrations and zlog.Error() if there are.
-func checkmig(db *sqlx.DB, migrations map[string][]byte, migpath string) error {
-	if migrations == nil && migpath == "" {
-		return nil
-	}
-
-	haveMig, ranMig, err := lsmig(db, migrations, migpath)
+func connectPostgreSQL(connect string) (*sqlx.DB, bool, error) {
+	db, err := sqlx.Connect("postgres", connect)
 	if err != nil {
-		return err
+		return nil, false, errors.Wrap(err, "connectPostgreSQL")
 	}
 
-	if d := diff(haveMig, ranMig); len(d) > 0 {
-		zlog.Errorf("pending migrations: %q", d)
-	}
-	if d := diff(ranMig, haveMig); len(d) > 0 {
-		zlog.Errorf("migrations in the DB that don't exist: %q", d)
-	}
-	return nil
+	db.SetMaxIdleConns(25) // Default 2
+	db.SetMaxOpenConns(25) // Default 0
+
+	return db, true, err // TODO: report if DB exists.
 }
 
-// diff returns a new slice with elements that are in "set" but not in "others".
-func diff(set []string, others ...[]string) []string {
-	var out []string
-	for _, setItem := range set {
-		found := false
-		for _, o := range others {
-			if sliceutil.InStringSlice(o, setItem) {
-				found = true
-				break
-			}
-		}
-
-		if !found {
-			out = append(out, setItem)
+func connectSQLite(connect string) (*sqlx.DB, bool, error) {
+	exists := true
+	if _, err := os.Stat(connect); os.IsNotExist(err) {
+		exists = false
+		err = os.MkdirAll(filepath.Dir(connect), 0755)
+		if err != nil {
+			return nil, false, errors.Wrap(err, "create DB dir")
 		}
 	}
-
-	return out
+	db, err := sqlx.Connect("sqlite3", connect)
+	return db, exists, errors.Wrap(err, "connectSQLite")
 }
-
-// Date formatted for SQL.
-func Date(t time.Time) string { return t.Format("2006-01-02 15:04:05") }
