efbef518450605840a25119b82220e72ba65b966
Martin Tournoij <martin@arp242.net>
2025-12-30 11:24:12 +0000

    Add Insert() and Update() functions
    To insert or update a row from a struct. I've copied these around a
    whole bunch of projects over the years because it never quite seemed to
    fit in zdb, but I'm somewhat tired of doing that and want one canonical
    version.

---
+++ insert.go
@@ -0,0 +1,200 @@
+package zdb
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"reflect"
+	"slices"
+	"strings"
+
+	"zgo.at/zstd/zreflect"
+)
+
+type (
+	Tabler interface {
+		// Table returns the table this row belongs to.
+		Table() string
+	}
+	Defaulter interface {
+		// Defaults sets default values for this row.
+		Defaults(context.Context)
+	}
+	Validator interface {
+		// Validate this row.
+		Validate(context.Context) error
+	}
+)
+
+func idcol(opts [][]string) (int, error) {
+	col := -1
+	for i, o := range opts {
+		if slices.Contains(o, "id") {
+			if col != -1 {
+				return -1, errors.New("more than one field with ,id option")
+			}
+			col = i
+		}
+	}
+	if col == -1 {
+		return col, errors.New("no ,id column")
+	}
+	return col, nil
+}
+
+// Insert all struct fields of t.
+//
+// Column names are taken from the db tag. Fields with the db tag set to "-" or
+// with the ",noinsert" option will be skipped.
+//
+// If a field has the ",id" option it will be fetched with a "returning" clause
+// and set.
+//
+// The Default() and Validator() methods will be called if t satisfies the
+// [Defaulter] or [Validator] interface.
+//
+// onConflict can contain an ON CONFLICT clause. This must include the ON
+// CONFLICT text itself. For example:
+//
+//	zdb.Insert(ctx, t, "on conflict (id) do update set data = tbl.data || excluded.data")
+func Insert(ctx context.Context, t Tabler, onConflict ...string) error {
+	if reflect.TypeOf(t).Kind() != reflect.Ptr {
+		return errors.New("zdb.Insert: t is not a pointer")
+	}
+
+	if d, ok := t.(Defaulter); ok {
+		d.Defaults(ctx)
+	}
+	if v, ok := t.(Validator); ok {
+		err := v.Validate(ctx)
+		if err != nil {
+			return err
+		}
+	}
+
+	cols, params, opts := zreflect.Fields(t, "db", "noinsert")
+
+	// Get the ID column, if any
+	idCol, err := idcol(opts)
+	if err != nil {
+		return fmt.Errorf("zdb.Insert: %w", err)
+	}
+	var (
+		idVal     any
+		idColName string
+	)
+	if idCol > -1 {
+		id := reflect.ValueOf(t).Elem().Field(idCol)
+		if !id.IsZero() {
+			return fmt.Errorf(`zdb.Insert: id field %q is not zero value but "%v"`, cols[idCol], id.Interface())
+		}
+
+		idVal, idColName = id.Addr().Interface(), cols[idCol]
+		params, cols = append(params[:idCol], params[idCol+1:]...), append(cols[:idCol], cols[idCol+1:]...)
+	}
+
+	for i := range cols {
+		cols[i] = quoteIdentifier(cols[i])
+	}
+	q := fmt.Sprintf(`insert into %s (%s) values (?) %s`,
+		quoteIdentifier(t.Table()),
+		strings.Join(cols, ", "),
+		strings.Join(onConflict, " "))
+	if idColName == "" {
+		err = Exec(ctx, q, params)
+	} else {
+		q += " returning " + quoteIdentifier(idColName)
+		err = Get(ctx, idVal, q, params)
+	}
+	if err != nil {
+		return fmt.Errorf("zdb.Insert: %w\n%s", err, q)
+	}
+	return nil
+}
+
+func quoteIdentifier(ident string) string {
+	var b strings.Builder
+	b.Grow(len(ident) + 2)
+	b.WriteByte('"')
+	for _, c := range ident {
+		if c == '"' {
+			b.WriteByte('"')
+		}
+		b.WriteRune(c)
+	}
+	b.WriteByte('"')
+	return b.String()
+}
+
+// UpdateAll signals that all columns should be updated.
+var UpdateAll = "\x00update\x00all\x00"
+
+// Update all the given columns. The column names should match the name of the
+// db tag.
+//
+// All columns in the struct wil be updated if [UpdateAll] is used as a column.
+// Fields with the db tag set to "-" or with the ",noinsert" or ",readonly"
+// option will be skipped.
+//
+// t needs to have a db tag with the ,id option set, which is used in the WHERE.
+//
+// The Default() and Validator() methods will be called if t satisfies the
+// [Defaulter] or [Validator] interface.
+func Update(ctx context.Context, t Tabler, columns ...string) error {
+	if reflect.TypeOf(t).Kind() != reflect.Ptr {
+		return errors.New("zdb.Update: t is not a pointer")
+	}
+	if len(columns) == 0 {
+		return errors.New("zdb.Update: no columns")
+	}
+
+	if d, ok := t.(Defaulter); ok {
+		d.Defaults(ctx)
+	}
+	if v, ok := t.(Validator); ok {
+		err := v.Validate(ctx)
+		if err != nil {
+			return err
+		}
+	}
+
+	var (
+		tbl              = t.Table()
+		cols, vals, opts = zreflect.Fields(t, "db", "noinsert")
+	)
+
+	idCol, err := idcol(opts)
+	if err != nil {
+		return fmt.Errorf("zdb.Update: %w", err)
+	}
+	if reflect.ValueOf(vals[idCol]).IsZero() {
+		return errors.New("zdb.Update: ID column is zero value")
+	}
+	where, whereParam := fmt.Sprintf(`%s = ?`, quoteIdentifier(cols[idCol])), vals[idCol]
+
+	var (
+		updateAll = len(columns) == 1 && columns[0] == UpdateAll
+		set       []string
+		params    []any
+	)
+	for i := range cols {
+		if i == idCol {
+			continue
+		}
+		if updateAll {
+			if !slices.Contains(opts[i], "readonly") {
+				set, params = append(set, quoteIdentifier(cols[i])+` = ?`), append(params, vals[i])
+			}
+		} else if slices.Contains(columns, cols[i]) {
+			set, params = append(set, quoteIdentifier(cols[i])+` = ?`), append(params, vals[i])
+		}
+	}
+
+	q := fmt.Sprintf(`update %s set %s where %s`,
+		quoteIdentifier(tbl), strings.Join(set, ", "), where)
+	err = Exec(ctx, q, append(params, whereParam)...)
+	if err != nil {
+		return fmt.Errorf("zdb.Update: %w", err)
+	}
+	return nil
+}
---
+++ test/insert_test.go
@@ -0,0 +1,204 @@
+package zdb_test
+
+import (
+	"context"
+	"testing"
+
+	"zgo.at/zdb"
+	"zgo.at/zstd/ztest"
+)
+
+func testTable(ctx context.Context, t *testing.T) {
+	t.Helper()
+	q := `create table tbl (id serial, str text, "NoTag" text)`
+	if zdb.SQLDialect(ctx) == zdb.DialectSQLite {
+		q = `create table tbl (id integer primary key autoincrement, str text, "NoTag" text)`
+	}
+	err := zdb.Exec(ctx, q)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+type insertRow struct {
+	ID             int    `db:"id,id"`
+	Str            string `db:"str"`
+	NoTag          string
+	NoInsert       string `db:"other,noinsert"`
+	Dash           string `db:"-"`
+	unExport1      string
+	defaultCalled  bool
+	validateCalled bool
+}
+
+func (insertRow) Table() string                     { return "tbl" }
+func (r *insertRow) Defaults(context.Context)       { r.defaultCalled = true }
+func (r *insertRow) Validate(context.Context) error { r.validateCalled = true; return nil }
+
+func TestInsert(t *testing.T) {
+	zdb.RunTest(t, func(t *testing.T, ctx context.Context) {
+		testTable(ctx, t)
+
+		row := insertRow{0, "aaa", "bbb", "xxx", "xxx", "xxx", false, false}
+
+		{ // Insert should work
+			err := zdb.Insert(ctx, &row)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if row.ID != 1 {
+				t.Fatalf("row.ID is not 1: %d", row.ID)
+			}
+			want := "id  str  NoTag\n1   aaa  bbb\n"
+			if have := zdb.DumpString(ctx, "select * from tbl"); have != want {
+				t.Fatal(have)
+			}
+			if !row.defaultCalled {
+				t.Fatal("Defaults() not called")
+			}
+			if !row.validateCalled {
+				t.Fatal("Validate() not called")
+			}
+		}
+
+		{ // Fail if ID not zero value
+			err := zdb.Insert(ctx, &row)
+			if !ztest.ErrorContains(err, "not zero value") {
+				t.Fatal(err)
+			}
+			if row.ID != 1 {
+				t.Fatalf("row.ID is not 1: %d", row.ID)
+			}
+			want := "id  str  NoTag\n1   aaa  bbb\n"
+			if have := zdb.DumpString(ctx, "select * from tbl"); have != want {
+				t.Fatal(have)
+			}
+		}
+
+		{ // Fail on non-ptr
+			err := zdb.Insert(ctx, row)
+			if !ztest.ErrorContains(err, "not a pointer") {
+				t.Fatal(err)
+			}
+			want := "id  str  NoTag\n1   aaa  bbb\n"
+			if have := zdb.DumpString(ctx, "select * from tbl"); have != want {
+				t.Fatal(have)
+			}
+		}
+	})
+}
+
+type updateRow struct {
+	ID             int    `db:"id,id"`
+	Str            string `db:"str"`
+	NoTag          string
+	NoInsert       string `db:"other,noinsert"`
+	Dash           string `db:"-"`
+	unExport1      string
+	defaultCalled  bool
+	validateCalled bool
+}
+
+func (updateRow) Table() string                     { return "tbl" }
+func (r *updateRow) Defaults(context.Context)       { r.defaultCalled = true }
+func (r *updateRow) Validate(context.Context) error { r.validateCalled = true; return nil }
+
+type updateReadOnlyRow struct {
+	ID    int    `db:"id,id"`
+	Str   string `db:"str,readonly"`
+	NoTag string
+}
+
+func (updateReadOnlyRow) Table() string { return "tbl" }
+
+func TestUpdate(t *testing.T) {
+	zdb.RunTest(t, func(t *testing.T, ctx context.Context) {
+		testTable(ctx, t)
+
+		{ // ID is zero value
+			row := updateRow{0, "aaa", "bbb", "xxx", "xxx", "xxx", false, false}
+			err := zdb.Update(ctx, &row, "str")
+			if !ztest.ErrorContains(err, "zero value") {
+				t.Fatal(err)
+			}
+		}
+
+		{ // Fail on non-ptr
+			row := updateRow{0, "aaa", "bbb", "xxx", "xxx", "xxx", false, false}
+			err := zdb.Update(ctx, row, "str")
+			if !ztest.ErrorContains(err, "not a pointer") {
+				t.Fatal(err)
+			}
+		}
+
+		row := updateRow{0, "aaa", "bbb", "xxx", "xxx", "xxx", false, false}
+		err := zdb.Insert(ctx, &row)
+		if err != nil {
+			t.Fatal(err)
+		}
+		row.defaultCalled, row.validateCalled = false, false
+
+		{ // No columns
+			err = zdb.Update(ctx, &row)
+			if !ztest.ErrorContains(err, "no columns") {
+				t.Fatal(err)
+			}
+		}
+
+		{ // One column
+			row.Str = "str changed"
+			err = zdb.Update(ctx, &row, "str")
+			if err != nil {
+				t.Fatal(err)
+			}
+			want := "id  str          NoTag\n1   str changed  bbb\n"
+			if have := zdb.DumpString(ctx, "select * from tbl"); have != want {
+				t.Fatal("\n" + have)
+			}
+			if !row.defaultCalled {
+				t.Fatal("Defaults() not called")
+			}
+			if !row.validateCalled {
+				t.Fatal("Validate() not called")
+			}
+		}
+
+		{ // Two columns
+			row.Str = "str changed2"
+			row.NoTag = "notag changed"
+			err = zdb.Update(ctx, &row, "str", "NoTag")
+			if err != nil {
+				t.Fatal(err)
+			}
+			want := "id  str           NoTag\n1   str changed2  notag changed\n"
+			if have := zdb.DumpString(ctx, "select * from tbl"); have != want {
+				t.Fatal("\n" + have)
+			}
+		}
+
+		{ // UpdateAll
+			row.Str = "all1"
+			row.NoTag = "all2"
+			err = zdb.Update(ctx, &row, zdb.UpdateAll)
+			if err != nil {
+				t.Fatal(err)
+			}
+			want := "id  str   NoTag\n1   all1  all2\n"
+			if have := zdb.DumpString(ctx, "select * from tbl"); have != want {
+				t.Fatal("\n" + have)
+			}
+		}
+
+		{ // Readonly
+			rowRo := updateReadOnlyRow{ID: row.ID, Str: "ro1", NoTag: "ro2"}
+			err = zdb.Update(ctx, &rowRo, zdb.UpdateAll)
+			if err != nil {
+				t.Fatal(err)
+			}
+			want := "id  str   NoTag\n1   all1  ro2\n"
+			if have := zdb.DumpString(ctx, "select * from tbl"); have != want {
+				t.Fatal("\n" + have)
+			}
+		}
+	})
+}
